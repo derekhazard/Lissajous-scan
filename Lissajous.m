326be461687d441411ab43b46d41c193cdae5a16
Derek Hazard <derek.hazard@gmail.com>
2018-03-30 21:08:17 -0400

    Second update to function for finding curve intersections

--- Lissajous.m
+++ Lissajous.m
@@ -7,13 +7,13 @@ y0 = 0;         % y initial position (10^-6 m)
 A = 1;          % x peak to peak (10^-6 m)
 B = 1;          % y peak to peak (10^-6 m)
 fxMin = 1;      % minimum integer x-frequency to test (Hz)
-fxMax = 10;      % maximum integer x-frequency to test (Hz)
-fyMin = 2;      % minimum integer y-frequency to test (Hz)
-fyMax = 10;      % maximum integer y-frequency to test (Hz)
+fxMax = 10;     % maximum integer x-frequency to test (Hz)
+fyMin = 1;      % minimum integer y-frequency to test (Hz)
+fyMax = 10;     % maximum integer y-frequency to test (Hz)
 dt = 0.001;     % time step size (s)
 tMin = 0.000;   % start time (s)
 tMax = 1.000;   % end time (s)
-p = 15;         % number of decimal places relevant to calculation
+p = 6;          % number of decimal places relevant to calculation
 
 % Generate data points for t, x, y, vx, vy, and VMag for each fx, fy pair
 % using nested for-loops.
@@ -21,28 +21,20 @@ for fx = fxMin:fxMax
     for fy = fyMin:fyMax
 
         % Setup input vectors
-        xi0 = [x0 y0];                      % x,y initial position vector
-        Ai = [A B];                         % x,y amplitudes vector
-        fi = [fx fy];                       % fx,fy frequencies vector
-        tMax = lcm(fx,fy)/(fx*fy); % calculate scan time
-        t = tMin:dt:tMax;          % time step vector
+        xi0 = [x0 y0];              % x,y initial position vector
+        Ai = [A B];                 % x,y amplitudes vector
+        fi = [fx fy];               % fx,fy frequencies vector
+        tMax = lcm(fx,fy)/(fx*fy);  % calculate scan time
+        t = tMin:dt:tMax;           % time step vector
         
         % Calculate data points for specified fx and fy.
-        xi = Xi(xi0,Ai,fi,t);      % x,y-position points
-        vi = Vi(Ai,fi,t);          % x,y-velocity component points
-        v  = VMag(vi);             % speed
+        xi = Xi(xi0,Ai,fi,t);       % x,y-position points
+        vi = Vi(Ai,fi,t);           % x,y-velocity component points
+        v  = VMag(vi);              % speed
         
         % Calculate intersection points
         xInter = intersections(xi,p);
-        
-%         Animated drawing of Lissajous curves
-%         a1 = animatedline('Color',[0 .7 .7]);
-%         axis([-0.5 0.5 -0.5 0.5])
-%         for i = 1:length(t)
-%             addpoints(a1,xi(1,i),xi(2,i));
-%             drawnow %limitrate
-%         end
-%         hold on
+
         % Plot scan and intersection points of scan curve.
         plot(xi(1,:),xi(2,:))
         xlabel(fx)
@@ -51,8 +43,7 @@ for fx = fxMin:fxMax
         if sum(size(xInter))>0
             scatter(xInter(1,:),xInter(2,:))
         end
-        
-
+        pause;
         hold off
     end
 end
@@ -193,7 +184,7 @@ end
 function xInter = intersections(xi1,p)
     
     xi1 = round(xi1,p);
-    
+
     % Intialize an empty return matrix
     xInter =[];
     
@@ -215,6 +206,29 @@ function xInter = intersections(xi1,p)
         error(message)
     end
     
+    % Check to see if the curve has overlapping sections.  If there is
+    % overlap, select a subset that is representative of the entire curve,
+    % but does not have overlap. Perform the intersection analysis on this
+    % subset.
+    %
+    % Store the indices where the curve starts to double over itself.
+    turn_indices = [];
+    for i=2:(c-1)
+        % Check to see if the point before and after each step is equal to
+        % see if the curve overlaps.
+        if ~round(xi1(1,i-1) - xi1(1,i+1), p) && ~round(xi1(2,i-1) - xi1(2,i+1), p)
+            turn_indices = [turn_indices, i];
+        end
+        
+    end
+    
+    % If the curve overlaps, select a representative subset and reset the
+    % size of the data set.
+    if length(turn_indices) > 1
+        xi1 = xi1(:, turn_indices(1):turn_indices(2));
+        [r c] = size(xi1);
+    end
+   
     % Copy input matrix and shift first data point the last. The curves are
     % supposed to be periodic, so the last line segment connects to the
     % first. This new matrix contains the line segment endpoints.
@@ -223,10 +237,10 @@ function xInter = intersections(xi1,p)
     % Simulatenously calculate all of the line segment slopes.
     dx = xi1(1,:) - xi2(1,:);
     dy = xi1(2,:) - xi2(2,:);
-    m = round(dy./dx,p);
+    m = round(dy./dx, p);
     
     % Simulateneously calculate all of the line segment y-intercept points.
-    b = round(xi1(2,:) - m.*xi1(1,:),p);
+    b = round(xi1(2,:) - m.*xi1(1,:), p);
     
     % Use a double for-loop to check if line segments intersect. The first
     % for-loop runs through every line segment.
@@ -284,19 +298,12 @@ function xInter = intersections(xi1,p)
             % intersection point.  If the difference between the slopes of
             % the lines is zero or both slopes are infinite, then they're 
             % parallel.
-            
-            v1 = [dx(i), dy(i)];
-            v2 = [dx(j); dy(j)];
-            theta = round(abs(acos((v1*v2)/(norm(v1)*norm(v2))))*180/pi,p);
-            if theta > 90
-                theta = 180 - theta;
-            end
             delta_m = round(m(j) - m(i),p); % Difference of line slopes
             
             % Scenario 1:
             % Check to see if both line segments are not verticle or
             % parallel to each other.  If not, determine the intercept.
-            if abs(m(i)) ~= Inf && abs(m(j)) ~= Inf && theta > 3
+            if abs(m(i)) ~= Inf && abs(m(j)) ~= Inf && delta_m ~= 0
                 x_inter = -(b(j) - b(i))/delta_m;   % x-intercept point
                 y_inter = m(i)*x_inter + b(i);      % y-intercept point
             %
@@ -334,9 +341,6 @@ function xInter = intersections(xi1,p)
             y_condition2 = (y_inter <= y_upper2) && (y_inter >= y_lower2);
             if x_condition1 && y_condition1 && x_condition2 && y_condition2
                 xInter = [xInter, [x_inter; y_inter]];
-                theta
-                m(i)
-                m(j)
             end
         end
     end
